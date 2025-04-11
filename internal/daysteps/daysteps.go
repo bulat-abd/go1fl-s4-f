9732ee58c7e2a0946522d098f7d45e7e271ebe74
Bulat <babdullatypov@gmail.com>
2025-04-11 23:23:18 +0300

    Refactoring: change var name for uniformity

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -18,18 +18,18 @@ const (
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-	list := strings.Split(data, ",")
-	if len(list) != 2 {
+	fields := strings.Split(data, ",")
+	if len(fields) != 2 {
 		return 0, time.Duration(0), fmt.Errorf("Bad format: exactly one comma must be present, separating exactly two fields")
 	}
-	steps, err := strconv.Atoi(list[0])
+	steps, err := strconv.Atoi(fields[0])
 	if err != nil {
 		return 0, time.Duration(0), fmt.Errorf("Bad format: could not parse int in the first field")
 	}
 	if steps <= 0 {
 		return 0, time.Duration(0), fmt.Errorf("Bad data: step count must be positive")
 	}
-	duration, err := time.ParseDuration(list[1])
+	duration, err := time.ParseDuration(fields[1])
 	if err != nil {
 		return 0, time.Duration(0), fmt.Errorf("Bad format: could not parse duration")
 	}
