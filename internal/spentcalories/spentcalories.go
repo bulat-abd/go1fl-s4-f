a9aa7e32061e2335b3b086b9dda8cc7046d2c769
Bulat <babdullatypov@gmail.com>
2025-04-13 20:19:58 +0300

    Remove comment

--- internal/spentcalories/spentcalories.go
+++ internal/spentcalories/spentcalories.go
@@ -47,7 +47,6 @@ func distance(steps int, height float64) float64 {
 }
 
 func meanSpeed(steps int, height float64, duration time.Duration) float64 {
-	// TODO: реализовать функцию
 	if duration <= 0 {
 		return 0
 	}
