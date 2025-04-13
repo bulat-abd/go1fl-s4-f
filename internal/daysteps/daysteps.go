12fc597b0d77ce67975d4fd9a4ddc531ced83f97
Bulat <babdullatypov@gmail.com>
2025-04-13 20:18:52 +0300

    Wrap errors while parsing data in daysteps package functions

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -24,14 +24,14 @@ func parsePackage(data string) (int, time.Duration, error) {
 	}
 	steps, err := strconv.Atoi(fields[0])
 	if err != nil {
-		return 0, 0, fmt.Errorf("bad format: could not parse int in the first field")
+		return 0, 0, fmt.Errorf("bad format: could not parse int in the first field - %w", err)
 	}
 	if steps <= 0 {
 		return 0, 0, fmt.Errorf("bad data: step count must be positive")
 	}
 	duration, err := time.ParseDuration(fields[1])
 	if err != nil {
-		return 0, 0, fmt.Errorf("bad format: could not parse duration")
+		return 0, 0, fmt.Errorf("bad format: could not parse duration - %w", err)
 	}
 	if duration <= 0 {
 		return 0, 0, fmt.Errorf("bad data: duration must be positive")
